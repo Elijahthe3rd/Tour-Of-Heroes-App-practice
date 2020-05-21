4fa075a6ee1fb4bb16fea938efebebfbd9c82f9e
Elijahthe3rd <sepuru.ne@yahoo.com>
2020-05-22 00:05:43 +0200

    first tour of heroes commint
    Signed-off-by: Elijahthe3rd <sepuru.ne@yahoo.com>

---
+++ src/app/mock-heros.ts
@@ -0,0 +1,20 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[]=[
+    {   id:23,
+        name:"Clive"
+    },
+    {
+        id:25,
+        name:"Hector"
+    },
+    {
+        id:27,name:"Elijah"
+    },
+    {
+        id:34,name:"Altaff"}
+    ,{
+        id:41,
+        name:"Japhta"
+    }
+    ];
